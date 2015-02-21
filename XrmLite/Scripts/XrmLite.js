c85c3b83b8b677552d3a0d752df2c6f42856ac93
jesse.x.jin@gmail.com <jesse.x.jin@gmail.com>
2015-02-21 13:54:21 -0700

    js file

---
+++ XrmLite/Scripts/XrmLite.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('[data-toggle="tabajax"]').click(function (e) {
+        e.preventDefault()
+        var loadurl = $(this).attr('href')
+        var targ = $(this).attr('data-target')
+        $.get(loadurl, function (data) {
+            $(targ).html(data);
+        });
+        $(this).tab('show');
+    });
+});
+
